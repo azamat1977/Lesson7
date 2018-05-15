3a4f9eb5f914cf8673ae948b3f29b9235310543f
azamat1977 <aza.azem@gmail.com>
2018-05-15 15:43:23 +0600

    file 1 for Lesson 7

---
+++ 1.py
@@ -0,0 +1,7 @@
+#1. to open particular file.txt navigate from Terminal to that folder, exc:
+#        cd PycharmProjects/Lesson_7
+#2. In Terminal type:
+#    f = open('test_open.txt', 'r')
+#   f.open()
+# file will opened to read
+
